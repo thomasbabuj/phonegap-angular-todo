6f619443d20870d9aab4c01cde73a3e14f6b354b
Thomas Babu <tommy.developer@gmail.com>
2014-02-01 14:29:57 +0800

    added the list and TodoCtl

--- www/js/app.js
+++ www/js/app.js
@@ -1,3 +1,11 @@
 
 // Create a new AngularJS module and tell Angular to initialize initialize
-angular.module('todo', ['ionic']);
+angular.module('todo', ['ionic'])
+	   .controller('TodoCtrl', function($scope){
+	   		$scope.tasks = [
+	   			{title : 'Collect coins'},
+	   			{title : 'Eat mushrooms'},
+	   			{title : 'Get high enough to grab the flag'},
+	   			{title : 'Find the Princess'}
+	   		];
+	   });
