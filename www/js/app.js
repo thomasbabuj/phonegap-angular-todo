78d3f4cd1d2a0cb26b74fe6c6bf8192cbd9422b8
Thomas Babu <tommy.developer@gmail.com>
2014-02-01 15:04:10 +0800

    Added a simple Modal window to create new task

--- www/js/app.js
+++ www/js/app.js
@@ -1,11 +1,31 @@
 
 // Create a new AngularJS module and tell Angular to initialize initialize
 angular.module('todo', ['ionic'])
-	   .controller('TodoCtrl', function($scope){
-	   		$scope.tasks = [
-	   			{title : 'Collect coins'},
-	   			{title : 'Eat mushrooms'},
-	   			{title : 'Get high enough to grab the flag'},
-	   			{title : 'Find the Princess'}
-	   		];
+	   .controller('TodoCtrl', function($scope, $ionicModal){
+	   		$scope.tasks = [];
+
+	   		//Create and load the Modal
+	   		$ionicModal.fromTemplateUrl('new-task.html', function(modal){
+	   			$scope.taskModal = modal;
+			}, {
+				scope : $scope,
+				animation : 'slide-in-up'
+	   		});
+
+	   		//Called when the form is submitted
+	   		$scope.createTask = function(task){
+	   			$scope.tasks.push(task);
+	   			$scope.taskModal.hide();
+	   		};
+
+	   		//Open our new task modal
+	   		$scope.newTask = function(){
+	   			$scope.taskModal.show();
+	   		};
+
+	   		//Close the new task modal
+	   		$scope.closeNewTask = function(){
+	   			$scope.taskModal.hide();
+	   		};
+
 	   });
