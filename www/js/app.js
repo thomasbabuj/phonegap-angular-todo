3830ea9df1a23bf5e86888e199a269c6d5f6d505
Thomas Babu <tommy.developer@gmail.com>
2014-02-01 15:51:01 +0800

    Added support for adding and selecting projects

--- www/js/app.js
+++ www/js/app.js
@@ -1,31 +1,117 @@
 
 // Create a new AngularJS module and tell Angular to initialize initialize
 angular.module('todo', ['ionic'])
-	   .controller('TodoCtrl', function($scope, $ionicModal){
-	   		$scope.tasks = [];
+/*
+* The Projects factory handels saving and loading projects from local storage and
+* also lets us save and load the last active project index.
+*/
+.factory('Projects', function(){
+	return {
+		all : function() {
+			var projectString = window.localStorage['projects'];
+			if(projectString) {
+				return angular.fromJson(projectString);
+			}
+			return [];
+		},
+		save : function(projects) {
+			window.localStorage['projects'] = angular.toJson(projects);
+		},
+		newProject : function(projectTitle) {
+			// Add a new project
+			return {
+				title : projectTitle,
+				tasks: []
+			};
+		},
+		getLastActiveIndex: function() {
+			return parseInt( window.localStorage['lastActiveProject']) || 0;
+		},
+		setLastActiveIndex: function(index) {
+			window.localStorage['lastActiveProject'] = index;
+		}
+	}
+})
 
-	   		//Create and load the Modal
-	   		$ionicModal.fromTemplateUrl('new-task.html', function(modal){
-	   			$scope.taskModal = modal;
-			}, {
-				scope : $scope,
-				animation : 'slide-in-up'
-	   		});
+.controller('TodoCtrl' , function($scope, $timeout, $ionicModal, Projects){
 
-	   		//Called when the form is submitted
-	   		$scope.createTask = function(task){
-	   			$scope.tasks.push(task);
-	   			$scope.taskModal.hide();
-	   		};
+	// A utility function for creating a new project
+	// with the given projectTitle
+	var createProject = function(projectTitle) {
+		var newProject = Projects.newProject(projectTitle);
+		$scope.projects.push(newProject);
+		Projects.save($scope.projects);
+		$scope.selectProject( newProject, $scope.projects.length- 1);
+	}
 
-	   		//Open our new task modal
-	   		$scope.newTask = function(){
-	   			$scope.taskModal.show();
-	   		};
+	// Load or initialize projects
+	$scope.projects = Projects.all();
 
-	   		//Close the new task modal
-	   		$scope.closeNewTask = function(){
-	   			$scope.taskModal.hide();
-	   		};
+	//Grab the last ative, or the first project
+	$scope.activeProject = $scope.projects[Projects.getLastActiveIndex()];
 
-	   });
+	//Called to create a new project
+	$scope.newProject = function() {
+		var projectTitle = prompt('Project name');
+		if( projectTitle ) {
+			createProject(projectTitle);
+		}
+	};
+
+	// Called to select the given project
+	$scope.selectProject = function(project, index) {
+		$scope.activeProject = project;
+		Project.setLastActiveIndex(index);
+		$scope.sideMenuController.close();
+	};
+
+
+	// Create our model
+	$ionicModal.fromTemplateUrl('new-task.html', function(modal){
+		$scope.taskModal =modal;
+	 }, {
+	 	scope : $scope
+	 });
+
+	$scope.createTask = function(task) {
+		if(!$scope.activeProject) {
+			return;
+		}
+		$scope.activeProject.tasks.push({
+			title: task.title
+		});
+		$scope.taskModal.hide();
+
+		//Inefficient,  but save all the projects
+		Projects.save($scope.projects);
+
+		task.title = "";
+	};
+
+	$scope.newTask = function() {
+		$scope.taskModal.show();
+	};
+
+	$scope.closeNewTask = function() {
+		$scope.taskModal.hide();
+	};
+
+	$scope.toggleProjects = function() {
+		$scope.sideMenuController.toggleLeft();
+	};
+
+	// Try to create the first project, make sure to defer
+	// this by using $timeout so everything is initialized properly
+	
+	$timeout (function() {
+		if($scope.projects.length == 0) {
+			while( true ) {
+				var projectTitle = prompt('Your first project title:');
+				if(projectTitle) {
+					createProject(projectTitle);
+					break;
+				}
+			}
+		}
+	});
+})
